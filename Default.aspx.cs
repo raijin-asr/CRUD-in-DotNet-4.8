88a6739c64e6fba03cdf3bd96c2e54d737987e8d
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2023-02-22 10:18:18 +0545

    on progress

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -3,8 +3,6 @@ using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
@@ -25,17 +23,19 @@ namespace ReportApp
 
         public void Page_Load(object sender, EventArgs e)
         {
-            try
-            {
-                if (!IsPostBack)
-                {
-                    items = DisplayData();
-                }
-            }
-            catch (Exception)
-            {
+            items = DisplayData();
+            //try
+            //{
+            //    if (!IsPostBack)
+            //    {
 
-            }
+            //    }
+            //}
+
+            //catch (Exception)
+            //{
+
+            //}
         }
 
         public List<ItemDetail> DisplayData()
@@ -73,134 +73,131 @@ namespace ReportApp
                     tcell.Text = dtbl.Rows[i]["college"].ToString();
                     trow.Controls.Add(tcell);
 
+                    //Action part
                     String id_Crud = dtbl.Rows[i]["id"].ToString();
                     tcell = new TableCell();
                     String model_id = "id_Crud" + i.ToString();
 
                     HtmlInputButton btn_Edit = new HtmlInputButton("button");
-                    btn_Edit.ID = "EditButton";
+                    btn_Edit.ID = "EditButton" + i.ToString();
                     btn_Edit.Value = "Edit";
                     btn_Edit.Attributes.Add("class", "btn-success");
-                    btn_Edit.Attributes.Add("runat", "server");
                     btn_Edit.Attributes.Add("data-toggle", "modal");
                     btn_Edit.Attributes.Add("data-target", "#MainContent_" + model_id);
-                    //btn_Edit.Attributes.Add("AutoPostBack", "true");
-                    //btn_Edit.Click += (object sender, EventArgs e) => { Edit_Click(sender, e, id_Crud); };
-                    //button.ServerClick += new System.EventHandler(this.Button_Click);
-                    //tcell.Controls.Clear();
                     tcell.Controls.Add(btn_Edit);
 
+                    //modal part
                     // Create a new HtmlGenericControl.
                     HtmlGenericControl Modal_Edit = new HtmlGenericControl("div");
                     Modal_Edit.ID = model_id;
                     Modal_Edit.Attributes.Add("class", "modal fade");
                     Modal_Edit.Attributes.Add("tabindex", "-1");
-                    Modal_Edit.Attributes.Add("runat", "server");
                     Modal_Edit.Attributes.Add("role", "dialog");
                     Modal_Edit.Attributes.Add("aria-labelledby", "exampleModalLabel");
                     Modal_Edit.Attributes.Add("aria-hidden", "true");
-                    Modal_Edit.Style["Height"] = "100px";
-                    Modal_Edit.Style["Width"] = "100px";
+                    //Modal_Edit.Style["Height"] = "200px";
+                    //Modal_Edit.Style["Width"] = "80%";
 
-
-                    // Create a new HtmlGenericControl.
+                    //// Create a new HtmlGenericControl.
                     HtmlGenericControl Modal_Dialog = new HtmlGenericControl("div");
-                    Modal_Dialog.Attributes.Add("runat", "server");
                     Modal_Dialog.Attributes.Add("class", "modal-dailog");
                     Modal_Dialog.Attributes.Add("role", "document");
 
                     HtmlGenericControl Modal_Content = new HtmlGenericControl("div");
-                    Modal_Content.Attributes.Add("runat", "server");
                     Modal_Content.Attributes.Add("class", "modal-content");
 
                     HtmlGenericControl Modal_Header = new HtmlGenericControl("div");
                     Modal_Header.Attributes.Add("class", "modal-header");
 
                     HtmlGenericControl Modal_Title = new HtmlGenericControl("h5");
-                    Modal_Title.ID = "modal_title";
+                    Modal_Title.ID = "modal_title" + i.ToString();
                     Modal_Title.Attributes.Add("class", "modal-title");
-                    Modal_Title.InnerHtml = "Editing Data";
+                    Modal_Title.InnerHtml = "Editing Data...";
                     Modal_Header.Controls.Add(Modal_Title);
 
-                    HtmlInputButton Modal_Close = new HtmlInputButton("button");
+                    Button Modal_Close = new Button();
+                    //Modal_Close.Attributes.Add("type", "button");
                     Modal_Close.Attributes.Add("class", "close");
-                    Modal_Close.Attributes.Add("runat", "server");
-                    Modal_Close.Attributes.Add("data-dismiss", "modal");
-                    Modal_Close.Attributes.Add("aria-label", "Close");
+                    Modal_Close.Text = "X";
+                    //Modal_Close.Attributes.Add("data-dismiss", "modal");
+                    //Modal_Close.Attributes.Add("aria-label", "Close");
+                    Modal_Close.Click += (object sender, EventArgs e) => { Close_Btn_Modal(sender,e); };
 
                     HtmlGenericControl Modal_Span = new HtmlGenericControl("span");
                     Modal_Span.Attributes.Add("aria-hidden", "true");
-                    Modal_Span.InnerHtml = "&times;";
+                    Modal_Span.InnerHtml = "X";
                     Modal_Close.Controls.Add(Modal_Span);
                     Modal_Header.Controls.Add(Modal_Close);
-
-
                     Modal_Content.Controls.Add(Modal_Header);
 
                     HtmlGenericControl Modal_Body = new HtmlGenericControl("div");
                     Modal_Body.Attributes.Add("class", "modal-body");
 
-                    HtmlForm Modal_Form = new HtmlForm();
-                    Modal_Form.ID = "modal_form";
+                    //HtmlForm Modal_Form = new HtmlForm();
+                    //Modal_Form.ID = "modal_form";
                     HtmlGenericControl Modal_Label1 = new HtmlGenericControl("label");
                     Modal_Label1.InnerText = "Name";
-                    Modal_Form.Controls.Add(Modal_Label1);
+                    Modal_Body.Controls.Add(Modal_Label1);
 
-                    HtmlInputText text1 = new HtmlInputText();
-                    text1.Value = dtbl.Rows[i]["name"].ToString();;
-                    Modal_Form.Controls.Add(text1);
+                    HtmlInputText textName = new HtmlInputText();
+                    String id_update = dtbl.Rows[i]["id"].ToString();
+                    textName.Value = dtbl.Rows[i]["name"].ToString();
+                    textName.ID = "id_name_" + i.ToString(); 
+                    String name_Update = textName.Value;
+                    Modal_Body.Controls.Add(textName);
 
                     HtmlGenericControl Modal_Label2 = new HtmlGenericControl("label");
-                    Modal_Label2.InnerText = "Name";
-                    Modal_Form.Controls.Add(Modal_Label2);
+                    Modal_Label2.InnerText = "College";
+                    Modal_Body.Controls.Add(Modal_Label2);
 
-                    HtmlInputText text2 = new HtmlInputText();
-                    text2.Value = dtbl.Rows[i]["name"].ToString();
-                    Modal_Form.Controls.Add(text2);
+                    HtmlInputText textCollege = new HtmlInputText();
+                    textCollege.Value = dtbl.Rows[i]["college"].ToString();
+                    textCollege.ID = "id_college_" + i.ToString();
+                    String college_Update = textCollege.Value;
+                    Modal_Body.Controls.Add(textCollege);
 
-                    Modal_Body.Controls.Add(Modal_Form);
+                    //Modal_Body.Controls.Add(Modal_Form);
                     Modal_Content.Controls.Add(Modal_Body);
 
                     HtmlGenericControl Modal_Footer = new HtmlGenericControl("div");
                     Modal_Footer.Attributes.Add("class", "modal-footer");
-                    HtmlInputButton Modal_dismiss = new HtmlInputButton("button");
+
+                    Button Modal_dismiss = new Button();
                     Modal_dismiss.Attributes.Add("class", "btn btn-secondary");
-                    Modal_dismiss.Attributes.Add("runat", "server");
-                    Modal_dismiss.Attributes.Add("data-dismiss", "modal");
-                    Modal_dismiss.Value = "Close";
+                    //Modal_dismiss.Attributes.Add("data-dismiss", "modal");
+                    Modal_dismiss.Text = "Close";
+                    Modal_dismiss.Click += (object sender, EventArgs e) => { Close_Btn_Modal(sender,e); };
+
                     HtmlAnchor Modal_anchor = new HtmlAnchor();
                     Modal_anchor.Attributes.Add("class", "btn");
-                    Modal_anchor.Attributes.Add("runat", "server");
                     Modal_anchor.Attributes.Add("data-dismiss", "modal");
-                    Modal_anchor.ID = "closemodal";
+                    Modal_anchor.ID = "closemodal" + i.ToString();
                     Modal_dismiss.Controls.Add(Modal_anchor);
                     Modal_Footer.Controls.Add(Modal_dismiss);
 
                     Button Modal_Update = new Button();
                     Modal_Update.Attributes.Add("class", "btn btn-primary");
-                    Modal_Update.Attributes.Add("runat", "server");
                     Modal_Update.Text = "Update";
-                    Modal_Update.Click += (object sender, EventArgs e) => { Edit_Click(sender, e, id_Crud); };
+                    Modal_Update.ID = "UpdateButton" + i.ToString();
+                    Modal_Update.Click += (object sender, EventArgs e) => { Edit_Click(sender, e, i.ToString(), name_Update, college_Update); };
                     Modal_Footer.Controls.Add(Modal_Update);
 
                     Modal_Content.Controls.Add(Modal_Footer);
+
                     Modal_Dialog.Controls.Add(Modal_Content);
                     Modal_Edit.Controls.Add(Modal_Dialog);
-
                     tcell.Controls.Add(Modal_Edit);
+
                     trow.Controls.Add(tcell);
 
                     //delete
-                    String id_Delete = dtbl.Rows[i]["id"].ToString();
                     tcell = new TableCell();
+                    String id_Delete = dtbl.Rows[i]["id"].ToString();
                     Button btn_Delete = new Button();
                     btn_Delete.Text = "Delete";
+                    btn_Delete.ID = "DeleteButton"+i.ToString();
                     btn_Delete.Attributes.Add("class", "btn btn-danger");
-                    btn_Delete.Attributes.Add("runat", "server");
-                    //btn_Delete.Click += new EventHandler(Delete_Click(id));
                     btn_Delete.Click += (object sender, EventArgs e) => { Delete_Click(sender, e, id_Delete); };
-                    //OnClientClick = "this.form.reset();return false;"
-                    //btn_Delete.Attributes.Add("OnClick", "(() =>Delete_Click(id))");
                     tcell.Controls.Add(btn_Delete);
                     trow.Controls.Add(tcell);
 
@@ -216,11 +213,16 @@ namespace ReportApp
             return items;
         }
 
+        protected void Close_Btn_Modal(object sender, EventArgs e) { 
+
+            Response.Write("<script>window.location.href='Default.aspx';</script>");
+        }
 
         protected void Insert_Click(object sender, EventArgs e)
         {
             Connection connect = new Connection();
             connect.Connect();
+
             String id_Crud = txtId.Text;
             String name_Crud = txtName.Text;
             String college_Crud = txtCollege.Text;
@@ -239,8 +241,7 @@ namespace ReportApp
 
             }
             Label1.Text = "Inserted";
-            //Response.Write("<script>window.location.href='Default.aspx';</script>");
-
+            Response.Write("<script>window.location.href='Default.aspx';</script>");
         }
 
         protected void Delete_Click(object sender, EventArgs e, String id)
@@ -251,6 +252,7 @@ namespace ReportApp
             connect.Connect();
             try
             {
+                Console.WriteLine("Deleted");
                 NpgsqlCommand cmd = new NpgsqlCommand();
                 cmd.Connection = Connection.connect;
 
@@ -265,32 +267,31 @@ namespace ReportApp
 
             }
             Label1.Text = "Deleted";
-            // Response.Write("<script>window.location.href='Default.aspx';</script>");
+            Response.Write("<script>window.location.href='Default.aspx';</script>");
 
         }
-
-        //public string id_Crud { get; set; }
-
-        //public void Update()
-        //{ 
-        //Edit_Click(id_Crud);
-        //}
-
-        protected void Edit_Click(object sender, EventArgs e, String id_Crud)
+       
+        protected void Edit_Click(object sender, EventArgs e, String id, String name, String college)
         {
             Connection connect = new Connection();
             connect.Connect();
 
-            
-            String name_Crud = text1.Value;
-            String college_Crud = text2.value;
             try
             {
+                //String id_Up = id + i.ToString();
+                //Control myControl1 = FindControl("textName");
+                //name = (row.FindControl("txtName") as TextBox).Text;
+                //String update_name = (this.FindControl("id_name_" + id) as TextBox).Text;
+                //String update_name= txtModelName.Text;
+                //Control myControl2 = FindControl("textCollege");
+                //String update_college = (this.FindControl("id_college_" + id) as TextBox).Text;
+                //String update_college = txtModelCollege.Text;
+
 
                 NpgsqlCommand cmd = new NpgsqlCommand();
                 cmd.Connection = Connection.connect;
 
-                string query = "UPDATE public.\"tblTest\" SET name ='" + name_Crud + "',college ='" + college_Crud + "' where id='" + id_Crud + "'";
+                string query = "UPDATE public.\"tblTest\" SET name ='" + name + "',college ='" + college + "' where id='" + id + "'";
                 cmd.CommandText = query;
                 int exe = cmd.ExecuteNonQuery();
 
@@ -302,7 +303,7 @@ namespace ReportApp
 
             }
             Label1.Text = "Updated";
-            // Response.Write("<script>window.location.href='Default.aspx';</script>");
+            Response.Write("<script>window.location.href='Default.aspx';</script>");
 
         }
 
